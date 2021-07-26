43ad8b76814706a12178d398e648ec05b35776ea
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-07-26 16:35:55 +0800

    Implement call to GET /users on react app
    Also implemented CORS configuration

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+// CORS
+const cors = require("cors");
+app.use(
+  cors({
+    origin: process.env.ORIGIN_URL,
+    credentials: true,
+  })
+);
+
 app.get("/", async (req, res) => {
-    res.status(200).json({"test": "done"});
+  res.status(200).json({ test: "done" });
 });
 
 app.use(express.json());
---
+++ client/src/components/users.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const USERS_API = `${process.env.REACT_APP_API_URL}/users`;
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get(USERS_API).then((response) => setUsers(response.data));
+  }, []);
+
+  return (
+    <div>
+      <h3>Users:</h3>
+      <ul>
+        {users &&
+          users.map((user) => {
+            return <li key={user.id}>{user.username}</li>;
+          })}
+      </ul>
+    </div>
+  );
+};
+
+export default Users;
