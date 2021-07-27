09f4fa1cd9188221fbfcf4a4e678fbf728a33146
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-07-27 16:36:28 +0800

    Attempt to deploy full-stack app on Heroku
    Previous attempt was to deploy frontend on Netlify, and backend on
    Heroku.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require("path");
 
 // CORS
 const cors = require("cors");
@@ -17,8 +18,20 @@ app.get("/", async (req, res) => {
 
 app.use(express.json());
 
-// Knex demonstration
-const usersRouter = require("./routes/users.route");
-app.use("/users", usersRouter);
+// Routes
+const apiRouter = express.Router();
+app.use("/api", apiRouter);
+
+const usersRouter = require("./routes/users.route"); // Knex demonstration
+apiRouter.use("/users", usersRouter);
+
+// To allow us to deploy both front and backend on Heroku, instead of backend to Heroku and frontend to Netlify
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 module.exports = app;
