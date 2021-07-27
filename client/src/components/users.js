b737e615a781fb3dde2ec6ede3a93fb5bfcac77e
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-07-27 16:39:22 +0800

    Update call to GET /users with /api namespace

--- client/src/components/users.js
+++ client/src/components/users.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const USERS_API = `${process.env.REACT_APP_API_URL}/users`;
+const USERS_API = `${process.env.REACT_APP_API_URL}/api/users`;
 
 const Users = () => {
   const [users, setUsers] = useState([]);
