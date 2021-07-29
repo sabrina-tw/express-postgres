a426b8b5770de01a3874b36bcc66f0f8af883e32
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-07-29 17:06:37 +0800

    Fix hardcoded knexfile config for local db

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: "pg",
-    connection: "postgres://localhost/secrets",
+    connection: `postgres://localhost/${process.env.PG_DATABASE}`,
     migrations: {
       directory: "./db/migrations",
     },
